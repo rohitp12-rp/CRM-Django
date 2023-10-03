3b404dd56f2ec0fd3ab802126e583c5c8fa63e31
Rohit <rohit.p120101@gmail.com>
2023-10-03 11:21:53 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user = 'root',
+    passwd = 'rohit1234'
+)
+
+#creating cursor object
+cursor = dataBase.cursor()
+
+cursor.execute("CREATE DATABASE finco")
+
+print("created database finco")
+
